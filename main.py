1aa73db85f11519218b150e3c85f72e117bdedb6
bxia33 <bxia33@gatech.edu>
2016-08-25 17:01:23 -0500

    add some functions

--- main.py
+++ main.py
@@ -1,65 +1,26 @@
-import psutil, gc, numpy, pandas, ref_genome_sizes, csv
 from util import *
 from more_itertools import *
 
-## step 1 : parse the wig file and add index, and remove no signal band.
 
-## step 2 : sort based on signal value
+
+
+## step 1 : parse the wig file
+
+## step 2: check steps and make sure the step could be applied to all the samples
+
+##  step 3: add index, and remove no signal band based on step
+
+## step 4: sort based on signal value
 bash_sort(indexed_file_path)
 
-## step 3 : caulculate the average
-def calculate_average_signal(sorted_file_paths):
-    iters = []
-    result = []
-    size = 0
-    path_avg = "avg.txt"
-    output_file = open(path_avg, "w")
-    output_file.close()
-    for path in sorted_file_paths:
-        iters.append(generator(path))
-    while True:
-        values = [next(x, [0, 0])[0] for x in iters]
-        avg = sum(values)/len(values)
-        if avg != 0:
-            result.append(avg)
-            size += 1
-            if size > 1000000:
-                open_file = open(path_avg, "a")
-                writer = csv.writer(open_file)
-                writer.writerow(result)
-                del result[:]
-                gc.collect()
-        else:
-            break
-        if result:
-            open_file = open(path_avg, "a")
-            writer = csv.writer(open_file)
-            writer.writerow(result)
-            del result[:]
-            gc.collect()
-
-##  save the replaced value file
-def replace_avg(sorted_file_paths, average_path):
-    for path in sorted_file_paths:
-        result = []
-        sorted_file = generator(path)
-        average_file = generator(average_path)
-        while True:
-            cur_avg = next(average_file, 0)
-            if cur_avg != 0:
-                result.append([next(sorted_file)[1], cur_avg])
-            else:
-                break
-        if result:
-            open_file = open(replaced_file_path, "a")
-            writer = csv.writer(open_file)
-            writer.writerow(result)
-            del result[:]
-            gc.collect()
+## step 5 : caulculate the average
 
 
+## step 6 save the replaced value file
 
 
-## step 4 : sort the replaced file based on index
+## step 7 : sort the replaced file based on index
+
+bash_sort(replaced_file_paths)
 
 ## step 5 : recreate wig file
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
-import psutil, os, numpy, pandas
+import os, psutil, gc, numpy, pandas, csv
+import operator
+from ref_genome_sizes import *
 
 def generator(path):
     with open(path) as file:
@@ -18,4 +20,86 @@ def bash_sort(path, output_path, reverse=True, buffer_size=None, OS=None):
         buffer = str(buffer_size * 1000000)
         cmd = "sort -r -n -s -k1 -o "+ output_path +" --buffer-size="+ buffer + " --temporary-directory=temp " + path
     os.system(cmd)
+
+def calculate_average_signal(sorted_file_paths):
+    iters = []
+    result = []
+    size = 0
+    path_avg = "avg.txt"
+    output_file = open(path_avg, "w")
+    output_file.close()
+    for path in sorted_file_paths:
+        iters.append(generator(path))
+    while True:
+        values = [next(x, [0, 0])[0] for x in iters]
+        avg = sum(values)/len(values)
+        if avg != 0:
+            result.append(avg)
+            size += 1
+            if size > 1000000:
+                open_file = open(path_avg, "a")
+                writer = csv.writer(open_file)
+                writer.writerow(result)
+                del result[:]
+                gc.collect()
+        else:
+            break
+        if result:
+            open_file = open(path_avg, "a")
+            writer = csv.writer(open_file)
+            writer.writerow(result)
+            del result[:]
+            gc.collect()
+
+def replace_avg(sorted_file_paths, average_path):
+    for path in sorted_file_paths:
+        result = []
+        sorted_file = generator(path)
+        average_file = generator(average_path)
+        while True:
+            cur_avg = next(average_file, 0)
+            if cur_avg != 0:
+                result.append([next(sorted_file)[1], cur_avg])
+            else:
+                break
+        if result:
+            open_file = open(replaced_file_path, "a")
+            writer = csv.writer(open_file)
+            writer.writerow(result)
+            del result[:]
+            gc.collect()
+
+def output_wig_fixed(replaced_sorted_file_paths, ref_genome, step):
+    ref_genome_sizes = master_genome_sizes[ref_genome]
+    sorted_sizes = sorted(ref_genome_sizes.items(), key=operator.itemgetter(1))
+
+    for path in replaced_sorted_file_paths:
+        output_path = ""
+        output_file = open(output_path, "w")
+        output_file.close()
+
+        for chrom in sorted_sizes:
+            result = []
+            size = 0
+            file = open(output_path, "a")
+            file.write("   \n")
+            chrom_size = chrom[1][1]- chrom[1][0]
+            while chrom_size > 0:
+                chrom_size -= step
+                ### TODO:
+
+                ###
+        
+
+
+    pass
+
+def output_wig_vary(replaced_sorted_file_paths, ref_genome, step):
+    pass
+
+def output_wig(replaced_sorted_file_paths, format, ref_genome, step):
+    if format == "fixed":
+        output_wig_fixed(replaced_sorted_file_paths, ref_genome, step)
+    elif format == "variable":
+        output_wig_vary(replaced_sorted_file_paths, ref_genome, step)
 
