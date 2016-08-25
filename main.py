febafc479f34ee1171d2bd4fbeaff530906076c9
bxia33 <bxia33@gatech.edu>
2016-08-25 00:27:59 -0500

    open_file = open(path_avg, "a")             writer = csv.writer(open_file)             writer.writerow(result)             del result[:]             gc.collect()

--- main.py
+++ main.py
@@ -1,11 +1,64 @@
-import psutil, gc, numpy, pandas, ref_genome_sizes, util
-
+import psutil, gc, numpy, pandas, ref_genome_sizes, csv
+from util import *
+from more_itertools import *
 
 ## step 1 : parse the wig file and add index, and remove no signal band.
 
 ## step 2 : sort based on signal value
+bash_sort(indexed_file_path)
 
-## step 3 : caulculate the average and save the replaced value file
+## step 3 : caulculate the average
+def calculate_average_signal(sorted_file_paths):
+    iters = []
+    result = []
+    size = 0
+    path_avg = "avg.txt"
+    output_file = open(path_avg, "w")
+    output_file.close()
+    for path in sorted_file_paths:
+        iters.append(generator(path))
+    while True:
+        values = [next(x, [0, 0])[0] for x in iters]
+        avg = sum(values)/len(values)
+        if avg != 0:
+            result.append(avg)
+            size += 1
+            if size > 1000000:
+                open_file = open(path_avg, "a")
+                writer = csv.writer(open_file)
+                writer.writerow(result)
+                del result[:]
+                gc.collect()
+        else:
+            break
+        if result:
+            open_file = open(path_avg, "a")
+            writer = csv.writer(open_file)
+            writer.writerow(result)
+            del result[:]
+            gc.collect()
+
+##  save the replaced value file
+def replace_avg(sorted_file_paths, average_path):
+    for path in sorted_file_paths:
+        result = []
+        sorted_file = generator(path)
+        average_file = generator(average_path)
+        while True:
+            cur_avg = next(average_file, 0)
+            if cur_avg != 0:
+                result.append([next(sorted_file)[1], cur_avg])
+            else:
+                break
+        if result:
+            open_file = open(replaced_file_path, "a")
+            writer = csv.writer(open_file)
+            writer.writerow(result)
+            del result[:]
+            gc.collect()
+
+
+
 
 ## step 4 : sort the replaced file based on index
 
