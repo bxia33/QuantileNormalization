593d020dede0e7f154c6da07e1cd5bfef033fada
bxia33 <bxia33@gatech.edu>
2016-08-25 21:32:49 -0500

    update loadwig parser

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
-import os, psutil, gc, numpy, pandas, csv
+import os, psutil, gc, numpy, pandas, csv, gzip
 import operator
 from ref_genome_sizes import *
+from more_itertools import *
 
 def generator(path):
     with open(path) as file:
@@ -8,6 +9,31 @@ def generator(path):
             value, index = line.split()
             values = [float(value), int(index)]
             yield values
+
+def load_wig_fixed(path, ref_genome):
+    pass
+
+def load_wig_vary(path, ref_genome):
+    pass
+
+def load_wig(*files, ref_genome):
+    file_paths = list(files)
+    for path in file_paths:
+        if path[-3:] == 'wig':
+            open_file = open(path)
+        elif path[-6:] == 'wig.gz':
+            open_file = gzip.open(path)
+
+        for line in open_file:
+            if line[0] == 't':
+                continue
+            elif line[0:9]=='fixedStep':
+                load_wig_fixed(path, ref_genome)
+                break
+            elif line[0:3]=='var':
+                load_wig_vary(path, ref_genome)
+                break
+    return
 
 def bash_sort(path, output_path, reverse=True, buffer_size=None, OS=None):
     dir = os.path.dirname(path)
@@ -69,37 +95,52 @@ def replace_avg(sorted_file_paths, average_path):
             del result[:]
             gc.collect()
 
-def output_wig_fixed(replaced_sorted_file_paths, ref_genome, step):
+def output_wig_fixed(replaced_sorted_file_paths, ref_genome, step, span):
     ref_genome_sizes = master_genome_sizes[ref_genome]
     sorted_sizes = sorted(ref_genome_sizes.items(), key=operator.itemgetter(1))
 
     for path in replaced_sorted_file_paths:
-        output_path = ""
+        output_path = path[:-4] + "output.wig"
         output_file = open(output_path, "w")
         output_file.close()
+        iter = peekable(generator(path))
 
         for chrom in sorted_sizes:
             result = []
             size = 0
             file = open(output_path, "a")
-            file.write("   \n")
-            chrom_size = chrom[1][1]- chrom[1][0]
-            while chrom_size > 0:
+            file.write("fixedStep chrom="+chrom+" start=1  step="+str(step)+" span="+str(step)+"\n")
+            chrom_size = chrom[1][1]- chrom[1][0] + 1
+            position = chrom[1][0]
+            while chrom_size >= 0:
                 chrom_size -= step
-                ### TODO:
+                current = iter.peek()
+                if current[1] == position:
+                    current = iter.__next__()
+                    result.append([current[0]])
+                else:
+                    result.append([0])
+                position += step
+                size += 1
+                if size > 1000000:
+                    open_file = open(output_path, "a")
+                    writer = csv.writer(open_file)
+                    writer.writerow(result)
+                    del result[:]
+                    gc.collect()
+            if result:
+                open_file = open(output_path, "a")
+                writer = csv.writer(open_file)
+                writer.writerow(result)
+                del result[:]
+                gc.collect()
 
-                ###
-        
-
-
+def output_wig_vary(replaced_sorted_file_paths, ref_genome, step, span):
     pass
 
-def output_wig_vary(replaced_sorted_file_paths, ref_genome, step):
-    pass
-
-def output_wig(replaced_sorted_file_paths, format, ref_genome, step):
+def output_wig(replaced_sorted_file_paths, format, ref_genome, step, span):
     if format == "fixed":
-        output_wig_fixed(replaced_sorted_file_paths, ref_genome, step)
+        output_wig_fixed(replaced_sorted_file_paths, ref_genome, step, span)
     elif format == "variable":
-        output_wig_vary(replaced_sorted_file_paths, ref_genome, step)
+        output_wig_vary(replaced_sorted_file_paths, ref_genome, step, span)
 
